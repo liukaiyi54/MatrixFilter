a19a311147795a600c5a4ae0c19b3e47b3ec670d
Michael <liukaiyi54@gmail.com>
2017-07-31 10:09:48 +0800

    first commit

---
+++ MatrixFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatrixFilter
+//
+//  Created by Michael on 31/07/2017.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MatrixFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MatrixFilter
+//
+//  Created by Michael on 31/07/2017.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
